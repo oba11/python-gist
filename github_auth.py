a672293c169d39e0f8c3ad3289a0be8e1f67696a
Adam Baxter <voltagex@voltagex.org>
2012-08-11 01:05:36 +1000

    Made pylint happier, started documentation

--- github_auth.py
+++ github_auth.py
@@ -1,3 +1,5 @@
+import sys, os, subprocess
+
 try:
     import requests
 except ImportError:
@@ -9,26 +11,53 @@ except ImportError:
     print "Please pip install requests_oauth2"
     sys.exit(1)
 
+import json
 
-class github_auth(object):
-    def __init__(self,appName = None,appURL = None, ClientID = None, ClientSecret = None, username = None, password = None):
-        self.appName = appName
-        self.appURL = appURL
-        self.ClientID = ClientID
-        self.ClientSecret = ClientSecret
+class GitHubAuth(object):
+    """Class handling authentication to GitHub via OAuth/token request"""
+    def __init__(self, app_name = None, app_url = None, client_id = None, 
+                 client_secret = None, username = None, password = None):
+        """ Initialise a GitHubAuth object
+            Note either client_id and client_secret for OAuth or
+            username and password for token request via basic auth
+            are required
+
+            :param app_name: Application name, displayed on the user's Applications page
+            :type app_name: str.
+
+            :param app_url: Application URL, displayed on the user's Applications page
+            :type app_url: str.
+            
+            :param client_id: OAuth client ID
+            :type client_id: str.
+
+            :param client_secret: OAuth client secret
+            :type client_secret: str.
+
+            :param username: Username for basic auth/token request
+            :type username: str.
+
+            :param password: Password for basic auth/token request
+            :type password: str.          
+        """
+        self.app_name = app_name
+        self.app_url = app_url
+        self.client_id = client_id
+        self.client_secret = client_secret
         #todo, probably need to handle clienttoken here too
         self.username = username
         self.password = password
         self.session = None
 
-    def get_session_simple(self,scopes,token_auth_callback=None,username=None,password=None):
+    def get_session_simple(self, scopes, token_auth_callback=None, username=None, password=None):
+        """  """
         if token_auth_callback is None: #http://stackoverflow.com/questions/1802971/nameerror-name-self-is-not-defined
             token_auth_callback = self.console_authorise_callback
 
-        if self.ClientID is not None and self.ClientSecret is not None:
-            token = self.authorise_web(scopes,token_auth_callback)
+        if self.client_id is not None and self.client_secret is not None:
+            token = self.authorise_web(scopes, token_auth_callback)
         else:
-            token = self.authorise_password(username,password)
+            token = self.authorise_password(username, password)
         if token is not None:
             return self.get_session(token)
         else:
@@ -36,20 +65,26 @@ class github_auth(object):
          #todo: more checks
 
     def get_session(self, token):
-        auth_header = {"Authorization": str.format("token {0}",token)}
-        if self.session is None: self.session = requests.Session(headers=auth_header, verify=True)
+        auth_header = {"Authorization": str.format("token {0}", token)}
+        if self.session is None: 
+            self.session = requests.Session(headers=auth_header, verify=True)
         return self.session
 
-    def authorise_web(self,redir=None,scopes=None,callback_function=None):
-        auth_handler = OAuth2(self.ClientID, self.ClientSecret, "https://github.com/login/", redir, authorization_url='oauth/authorize', token_url='oauth/access_token')
-        get_auth_url = auth_handler.authorize_url(scopes)
-        code = callback_function()
+    def authorise_web(self, redir=None, scopes=None, callback_function=None):
+        auth_handler = OAuth2(self.client_id, 
+                              self.client_secret, 
+                              "https://github.com/login/", 
+                              redir, 
+                              authorization_url='oauth/authorize', 
+                              token_url='oauth/access_token')
+        auth_url = auth_handler.authorize_url(scopes)
+        code = callback_function(auth_url)
 
         if code != None:
             return code
         else: raise('Could not get OAuth authorisation code')
 
-    def authorise_password(self,username,password):
+    def authorise_password(self, username, password):
         response = requests.get('https://api.github.com/authorizations', auth=(username, password))
 
         if response.ok:
@@ -57,8 +92,8 @@ class github_auth(object):
             if token_response is not None:
                 return token_response
             else:
-                return self.get_new_token((username,password))
-        elif response.status_code==401:
+                return self.get_new_token((username, password))
+        elif response.status_code == 401:
             raise("Username or password incorrect, try again.")
         #    
         #else:
@@ -67,13 +102,13 @@ class github_auth(object):
 
     def get_existing_token(self, response):
         for auth_dictionary in response.json:
-            if self.appName in auth_dictionary['app']['name']:
+            if self.app_name in auth_dictionary['app']['name']:
                 return auth_dictionary['token']
         return None
         
-    def get_new_token(self,cached_credentials):
+    def get_new_token(self, cached_credentials):
         headers = {'accept': 'application/json'}
-        payload = {'scopes': 'gist', 'note': 'Requested by ' + self.appName, 'note_url': self.appURL}
+        payload = {'scopes': 'gist', 'note': 'Requested by ' + self.app_name, 'note_url': self.app_url}
         response = requests.post('https://api.github.com/authorizations', auth=cached_credentials, data=json.dumps(payload), headers=headers)
         
         if response.ok:
@@ -81,19 +116,21 @@ class github_auth(object):
         else:
             raise(str.format("Couldn't get client token - {0}: {1}", response.status_code, response.text)) #yes, this is likely to be a json response but I want to make sure I see the error    
 
-    def console_authorise_callback(self):
-        self.openbrowser(get_auth_url)
+    def console_authorise_callback(self, auth_url):
+        self.openbrowser(auth_url)
         code = raw_input('OK, if you have a code from the webpage, please enter it here: ')
         return code
 
-    def openbrowser(self,url):
-        friendly_message = "Press Enter if you'd like to go to " + url + " with your default browser to authorise this script on GitHub, otherwise hit ctrl/command-C now"
+    def openbrowser(self, url):
+        friendly_message = "Press Enter if you'd like to go to " + url + \
+                           "with your default browser to authorise this script on GitHub, \
+                            otherwise hit ctrl/command-C now"
         raw_input(friendly_message)
 
         #from http://stackoverflow.com/questions/1795111/is-there-a-cross-platform-way-to-open-a-file-browser-in-python
-        if sys.platform=='win32':
+        if sys.platform =='win32':
             os.startfile(url)
-        elif sys.platform=='darwin':
+        elif sys.platform == 'darwin':
             subprocess.Popen(['open', url])
         else:
             try:
--- python-gist.py
+++ python-gist.py
@@ -1,12 +1,8 @@
-import sys
-import os
-import subprocess
 import atexit
 
 import ConfigParser
 import getpass
-
-import json
+import sys
 
 try:
     import requests
@@ -19,14 +15,19 @@ except ImportError:
     print "Please pip install requests_oauth2"
     sys.exit(1)
 
-import github_auth
+import json
 
-class gist(object):
+from github_auth import GitHubAuth
+
+class Gist(object):
     OAUTH_CONFIG_FILENAME = 'oauth.cfg'
     CONFIG_FILENAME = 'defaults.cfg'
     
     def choose_authmethod(self):
-        choice = raw_input('Please select an authorisation method.\r\n1. Open a web browser, cut and paste the access token.\r\n2. Use your GitHub username and password to get a token automagically.\r\n(Default is 2): ')
+        choice = raw_input('Please select an authorisation method.\r\n \
+                            1. Open a web browser, cut and paste the access token.\r\n \
+                            2. Use your GitHub username and password to get a token automagically.\r\n \
+                            (Default is 2): ')
         choice = choice.strip()
         if choice == '2' or choice == '':
             self.authorise_password() 
@@ -41,32 +42,32 @@ class gist(object):
 
         self.config = ConfigParser.RawConfigParser()
         self.config.read([self.OAUTH_CONFIG_FILENAME])
-        self.config.filehandle = open(self.OAUTH_CONFIG_FILENAME,'a')
+        self.config.filehandle = open(self.OAUTH_CONFIG_FILENAME, 'a')
         
         if not self.config.has_section('Credentials'):
             self.config.add_section('Credentials')
 
-        self.ClientToken = self.config.get_quiet('Credentials','client_token')
-        self.ClientID = self.config.get_quiet('Credentials','client_id')
-        self.ClientSecret = self.config.get_quiet('Credentials','client_secret')
+        self.client_token = self.config.get_quiet('Credentials', 'client_token')
+        self.client_id = self.config.get_quiet('Credentials', 'client_id')
+        self.client_secret = self.config.get_quiet('Credentials', 'client_secret')
         self.redir_url = "http://voltagex.github.com/python-gists/oauth.html"
-        self.auth = github_auth.github_auth("python-gist","http://github.com/voltagex/python-gist",self.ClientID,self.ClientSecret)
+        self.auth = GitHubAuth("python-gist", "http://github.com/voltagex/python-gist", self.client_id, self.client_secret)
 
-        if self.ClientToken is None:
+        if self.client_token is None:
             self.choose_authmethod()
         #else:
         #    print "Loaded saved access token, I'm good to go." 
     
-    def authorise_web():
-        return self.auth.authorise_web(self,redir=self.redir_url,scopes="gist")
+    def authorise_web(self):
+        return self.auth.authorise_web(self, redir=self.redir_url, scopes="gist")
 
-    def authorise_password():
+    def authorise_password(self):
         username = raw_input("Please enter your username: ")
         password = getpass.getpass("Please enter your password: ")
-        return self.auth.authorise_password(username,password)
+        return self.auth.authorise_password(username, password)
 
 
-    def post_gist(self,description="Posted by python-gist", public=False, gist_files=None, content=None):
+    def post_gist(self, description="Posted by python-gist", public=False, gist_files=None, content=None):
         if content is not None:
             files = {"python-gist-post.txt": {"content": content}}
         elif gist_files is not None: 
@@ -78,29 +79,29 @@ class gist(object):
                     "public": public,
                     "files": files,
                    }
-        response = self.auth.get_session(self.ClientToken).post("https://api.github.com/gists", data=json.dumps(payload))
+        response = self.auth.get_session(self.client_token).post("https://api.github.com/gists", data=json.dumps(payload))
         if response.ok:
             return response.json['html_url']
         else: print "Error " + response.text
         return
     
     def cleanup(self):
-        self.config.set('Credentials', 'Client_ID', self.ClientID)
-        self.config.set('Credentials', 'Client_Secret', self.ClientSecret)
-        self.config.set('Credentials', 'Client_Token', self.ClientToken)
+        self.config.set('Credentials', 'Client_ID', self.client_id)
+        self.config.set('Credentials', 'Client_Secret', self.client_secret)
+        self.config.set('Credentials', 'Client_Token', self.client_token)
         self.config.write(self.config.filehandle)
         
         self.config.filehandle.flush()
         self.config.filehandle.close()
         
 if __name__ == "__main__":
-    def get_quiet(self,section,value):
-        if self.has_option(section,value):
-            return self.get(section,value)
+    def get_quiet(self, section, value):
+        if self.has_option(section, value):
+            return self.get(section, value)
         return None
     ConfigParser.RawConfigParser.get_quiet = get_quiet
     
-gist = gist()
+gist = Gist()
 if (sys.stdin):
     print gist.post_gist(content=sys.stdin.readlines())
     raw_input()
